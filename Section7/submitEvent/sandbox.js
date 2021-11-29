659d6291ebe208c2d0eff57decb1fa11b60a9a75
BryantLan12138 <bryant.lan1212@gmail.com>
2021-11-30 07:02:34 +1030

    building up the quiz app

--- Section7/submitEvent/sandbox.js
+++ Section7/submitEvent/sandbox.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.signup-form');
 const input = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 // console.log(form)
 form.addEventListener('submit', (e) => {
     // stop refreshing
@@ -15,15 +16,17 @@ form.addEventListener('submit', (e) => {
     console.log(result);
     if(result) {
         console.log('pass the test');
+        feedback.textContent = "The username is validated";
     } else {
         console.log('fail the test');
+        feedback.textContent = "You cannot use the username";
     }
 
     // 2nd method
-    let result = input.value.search(regex);
-    if(result != -1) {
-        console.log('pass the test');
-    } else {
-        console.log('fail the test');
-    }
+    // let result = input.value.search(regex);
+    // if(result != -1) {
+    //     console.log('pass the test');
+    // } else {
+    //     console.log('fail the test');
+    // }
 });
