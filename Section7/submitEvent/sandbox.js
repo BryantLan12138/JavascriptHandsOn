6aece0e53f0ecf27d3d3b9ae78020ddb5f79fef1
BryantLan12138 <bryant.lan1212@gmail.com>
2021-11-29 17:35:38 +1030

    create self designed regex expression

--- Section7/submitEvent/sandbox.js
+++ Section7/submitEvent/sandbox.js
@@ -6,4 +6,24 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(e);
     console.log(input.value);
+
+    // regex 
+    const regex = /^[a-z]{6,}$/;
+
+    // two methods to validate the input with regex
+    let result = regex.test(input.value);
+    console.log(result);
+    if(result) {
+        console.log('pass the test');
+    } else {
+        console.log('fail the test');
+    }
+
+    // 2nd method
+    let result = input.value.search(regex);
+    if(result != -1) {
+        console.log('pass the test');
+    } else {
+        console.log('fail the test');
+    }
 });
