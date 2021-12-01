e2de9663ec4f062062d8ef09ca6939cc1e6f9253
BryantLan12138 <bryant.lan1212@gmail.com>
2021-12-02 09:30:08 +1030

    practice callback functions

--- Section12/MakingHTTPRequest/sandbox.js
+++ Section12/MakingHTTPRequest/sandbox.js
@@ -1,10 +1,45 @@
-const request = new XMLHttpRequest();
-console.log(request)
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4) {
-        console.log(request.responseText);
-    }
+const asyncfunction = ((resource, callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200) {
+            // console.log(request.responseText);
+            let data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if(request.readyState === 4) {
+            // console.log('The API endpoint is not correct');
+            callback('API gate not reacheable', undefined);
+        }
+    });
+    
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', resource)
+    request.send();
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+// asyncfunction();
+
+// callback hell, nesting too much callback functions, need to use Promise 
+asyncfunction('test1.json', (err, data) => {
+    console.log('callback fired')
+    if(err === undefined) {
+        console.log(data)
+    } else {
+        console.log(err)
+    }
+    asyncfunction('test2.json', (err, data) =>{
+        console.log('callback fired')
+        if(err === undefined) {
+            console.log(data)
+        } else {
+            console.log(err)
+        }
+        asyncfunction('test3.json', (err, data) =>{
+            console.log('callback fired')
+            if(err === undefined) {
+                console.log(data)
+            } else {
+                console.log(err)
+            }
+        });
+    });
+});
